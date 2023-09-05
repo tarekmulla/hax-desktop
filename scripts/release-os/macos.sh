9a65dc1e355e961e2f9df860ea592da4dc77d2af
Tarek Mulla <tarek@mulla.au>
2023-09-05 12:42:10 +1000

    upload release scripts

---
+++ scripts/release-os/macos.sh
@@ -0,0 +1,37 @@
+echo "\n\nBundles the application and all its dependencies into a single package (.app)"
+pyinstaller --noconfirm --windowed --name="HaX" \
+            --icon="hax/static/images/icon.png" \
+            --add-data="hax/config.yml:." \
+            --add-data="hax/static:static" \
+            hax/main.py
+
+echo "\n\nCreate a release folder"
+mkdir -p release/macos
+rm -rf release/macos/*
+
+echo "\n\nCopy the app bundle to the release folder"
+cp -r "dist/HaX.app" release/macos
+
+echo "If the dmg already exists, delete it"
+test -f "release/HaX.dmg" && rm "release/HaX.dmg"
+
+echo "\n\nCreating the new release:"
+create-dmg \
+  --volname "HaX" \
+  --volicon "hax/static/images/icon.png" \
+  --window-pos 200 120 \
+  --window-size 600 300 \
+  --icon-size 100 \
+  --icon "HaX.app" 175 120 \
+  --hide-extension "HaX.app" \
+  --app-drop-link 425 120 \
+  "release/HaX.dmg" \
+  "release/macos/"
+
+echo "\n\nCleanup temp files"
+rm -rf dist
+rm -rf build
+rm -rf release/macos
+rm hax.spec
+
+echo "\n\nMacOS Release package (dmg) created successfully"
--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then
   echo "Releasing Linux version"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Releasing MacOS version (.dmg)"
-  ./scripts/release/macos.sh
+  ./scripts/release-os/macos.sh
 elif [[ "$OSTYPE" == "msys" ]]; then
   echo "Releasing Windows version (.exe)"
 fi

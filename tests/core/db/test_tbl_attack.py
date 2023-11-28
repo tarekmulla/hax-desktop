837cbd94bb796494fcdede04ea95ecd3aa03d162
Tarek Mulla <tarek@mulla.au>
2023-11-29 09:46:01 +1100

    fix the unit tests

--- tests/core/db/test_tbl_attack.py
+++ tests/core/db/test_tbl_attack.py
@@ -10,28 +10,28 @@ from . import FakeCursor
 
 
 def test_insert_attack(mocker):
-  mocker.patch('utilities.db.base.execuate', return_value=FakeCursor(1))
+  mocker.patch('core.utilities.db.base.execuate', return_value=FakeCursor(1))
   assert insert_attack("attack_url", RequestType.GET, AttackType.XSS, "attack_params") == True
 
 
 def test_update_attack(mocker):
-  mocker.patch('utilities.db.base.execuate', return_value=FakeCursor(1))
+  mocker.patch('core.utilities.db.base.execuate', return_value=FakeCursor(1))
   assert update_attack(0, "attack_url", RequestType.GET, AttackType.XSS, "attack_params") == True
 
 
 def test_get_attack_by_url(mocker):
-  mocker.patch('utilities.db.base.execuate', return_value=FakeCursor(1, ["TEST"]))
+  mocker.patch('core.utilities.db.base.execuate', return_value=FakeCursor(1, ["TEST"]))
   assert get_attack_by_url("attack_url") == "TEST"
 
 
 def test_get_attack_by_url_failure(mocker):
   with pytest.raises(SQLException):
-    mocker.patch('utilities.db.base.execuate', return_value=FakeCursor(2, ["TEST1", "TEST2"]))
+    mocker.patch('core.utilities.db.base.execuate', return_value=FakeCursor(2, ["TEST1", "TEST2"]))
     assert get_attack_by_url("attack_url")
 
 
 def test_get_all_attacks(mocker):
-  mocker.patch('utilities.db.base.execuate', return_value=FakeCursor(2, [
+  mocker.patch('core.utilities.db.base.execuate', return_value=FakeCursor(2, [
     [0,"",RequestType.GET,"",AttackType.XSS],
     [2,"",RequestType.GET,"",AttackType.XSS]])
   )

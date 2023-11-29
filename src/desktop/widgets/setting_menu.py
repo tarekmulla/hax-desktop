637eec260e80e1ce009802adf6dc9f4143a38980
Tarek Mulla <tarek@mulla.au>
2023-11-30 08:50:46 +1100

    fix linting issues

--- src/desktop/widgets/setting_menu.py
+++ src/desktop/widgets/setting_menu.py
@@ -1,9 +1,7 @@
 """The main menu of the application"""
-from customtkinter import CTkButton, CTkFrame, CTkImage
-from PIL import Image
+from customtkinter import CTkFrame
 
 from core.classes.gui.enums import Windows
-from core.utilities.config import get_image_path
 from desktop.widgets.classes.menu_item import MenuItem
 
 
@@ -17,15 +15,14 @@ class SettingMenu(CTkFrame):
   def init_items(self, event_func):
     """Initialize the menu items"""
     self.menu_items = [
-      MenuItem("General", "xss.png", Windows.GENERAL_SETTING),
-      MenuItem("Appearance", "xss.png", Windows.APPEARANCE_SETTING),
-      MenuItem("Cloud backend", "xss.png", Windows.CLOUD_SETTING)
+      MenuItem(self, "General", "xss.png", Windows.GENERAL_SETTING),
+      MenuItem(self, "Appearance", "xss.png", Windows.APPEARANCE_SETTING),
+      MenuItem(self, "Cloud backend", "xss.png", Windows.CLOUD_SETTING)
     ]
 
     row = 0
     for item in self.menu_items:
-      btn = self.create_menu_item(self, item.name, item.image, item.window, event_func)
-      btn.grid(row=row, column=0, sticky="nsew")
+      self.create_menu_item(item, row, event_func)
       row = row + 1
 
   def mouse_click(self, event, window: Windows, click_event_func):
@@ -35,11 +32,7 @@ class SettingMenu(CTkFrame):
     self.current_window = window
     click_event_func(event, window)
 
-  def create_menu_item(self, parent, text, image, window, click_event_func):
+  def create_menu_item(self, item, row, click_event_func):
     """Create Button as menu item"""
-    img = CTkImage(dark_image=Image.open(get_image_path(image)), size=(25, 25))
-    btn = CTkButton(parent, image=img, width=200, height=40, text=text, compound='left', anchor="w",
-                    corner_radius=0, border_spacing=10, fg_color="transparent",
-                    text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),)
-    btn.bind("<Button-1>", lambda e: self.mouse_click(e, window, click_event_func))
-    return btn
+    item.btn.bind("<Button-1>", lambda e: self.mouse_click(e, item.window, click_event_func))
+    item.btn.grid(row=row, column=0, sticky="nsew")
